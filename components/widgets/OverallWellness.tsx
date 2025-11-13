c9486b250c90ccf180bdac8057d81801323194b0
Your Name <your.email@example.com>
2025-11-12 23:17:18 -0600

    CCWAI 11122025 3

--- components/widgets/OverallWellness.tsx
+++ components/widgets/OverallWellness.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+import { TrendingUp, TrendingDown, Minus } from 'lucide-react'
 import { formatDate } from '@/lib/utils'
 
 interface CheckIn {
